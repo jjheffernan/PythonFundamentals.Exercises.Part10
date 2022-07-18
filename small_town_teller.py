c18f1058561b6d267807bda8166238eb9bbc45f4
jjheffernan <jjheffernan99@gmail.com>
2022-07-18 02:51:24 -0400

    added base functionalities to methods.

--- small_town_teller.py
+++ small_town_teller.py
@@ -5,24 +5,36 @@ small town teller example for zip code wilmington
 
 
 class Person:
-    id = 0
-    first_name = ''
-    last_name = ''
+
+    def __init__(self, name):
+        #  id_num: int, first_name: str, last_name: str
+        self.name = name
+        # person_id = id_num
+        # first_name = first_name
+        # last_name = last_name
 
 
 class Account:
-    number = 0
-    type = ''
-    owner = 0
-    balance = 0.0
+
+    def __init__(self):
+        # id_num: int, type: str, owner: Person
+        # account_num = id_num
+        # account_type = type
+        # account_owner = owner
+        self.balance = 0.0
 
 
 class Bank:
     """
     this might end us as an interface
     """
-    def add_customer(self, customer_id: int):
-        pass
+    def __init__(self, name, account_id):
+        self.name = name
+        self.account_id = account_id
+
+
+    def add_customer(self, customer_id: Person):
+
 
     def add_account(self, account_id: int):
         pass
@@ -31,10 +43,18 @@ class Bank:
         return None
 
     def deposit_funds(self, account_id: int, deposit: float):
-        pass
+
+        Account.balance += deposit
+        # pass  # escape return
 
     def withdraw_funds(self, account_id: int, withdraw: float):
-        pass
+        if Account.balance >= withdraw:
+            Account.balance -= withdraw
+            print(f"You withdrew: ${withdraw} \n")
+        else:
+            print("insufficient funds. ")
+        # pass  # escape return
 
     def get_balance(self, account_id: int):
-        pass
+
+        pass  # escape return
